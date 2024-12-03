31614473bd987e6f0a012aa08e8e6de0162b6295
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-03 14:22:46 +0000

    良い感じにbase書いた

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono, MiddlewareHandler } from 'hono'
+import { InteractionType, verifyKey } from 'discord-interactions';
+import { Interaction } from './types/interaction';
+import { verifyRequest, responsePing, logger } from './utils';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(logger(), verifyRequest(), responsePing())
+
+app.post("/", async (c) => {
+  const interaction = await c.req.json<Interaction>();
+  if (!interaction.data) return
+
+  /* if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    switch (interaction.data.name) {
+      case "ping":
+
+    }
+  } */
 })
+
+
 
 export default app

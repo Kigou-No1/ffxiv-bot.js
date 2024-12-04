6d2849b15b6df4be43db7d2eea2ce208601de2eb
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-04 10:30:52 +0000

    utilsからmiddlewareを分離した あとレスポンス返すためのユーティリティ書いた

--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,137 @@
-import { Context, Env, MiddlewareHandler } from "hono";
-import { Interaction } from "./types/interaction";
-import { verifyKey } from "discord-interactions";
+import {
+    APIEmbed,
+    APIEmbedAuthor,
+    APIEmbedField,
+    APIEmbedFooter,
+    APIEmbedImage,
+    APIEmbedThumbnail,
+    APIEmbedVideo,
+    APIInteractionResponseCallbackData,
+} from "discord-api-types/v10";
+import { InteractionResponseType } from "discord-interactions";
 
-export const verifyRequest = (): MiddlewareHandler => async (c, next) => {
-    const interaction = await c.req.json<Interaction>();
-    const signature = c.req.header()["X-Signature-Ed25519"];
-    const timestamp = c.req.header()["X-Signature-Timestamp"];
-    const raw = await c.req.raw.text();
-    const isValid = signature && timestamp && await verifyKey(raw, signature, timestamp, c.env.DISCORD_PUBLIC_KEY);
-    if (!isValid || !interaction.data) {
-        return c.text("invalid request signature", 401);
+export type APIResponse = {
+    type: InteractionResponseType;
+    data: APIInteractionResponseCallbackData;
+};
+
+export const response = (
+    type: InteractionResponseType,
+    data: APIInteractionResponseCallbackData,
+) => {
+    return {
+        type: type,
+        data: data,
+    } as APIResponse;
+};
+
+export const commandNotFoundResponse = () => {
+    const embed = new EmbedBuilder()
+        .setTitle("Command not found")
+        .setDescription("The command you are looking for does not exist.")
+        .build();
+
+    return response(InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, {
+        embeds: [embed],
+    });
+};
+
+class EmbedBuilder {
+    private embed: APIEmbed = {};
+
+    constructor(optitons?: {
+        title?: string;
+        description?: string;
+        url?: string;
+        timestamp?: string;
+        color?: number;
+        footer?: APIEmbedFooter;
+        image?: APIEmbedImage;
+        thumbnail?: APIEmbedThumbnail;
+        author?: APIEmbedAuthor;
+        fields?: APIEmbedField[];
+        video?: APIEmbedVideo;
+    }) {
+        this.embed = {
+            ...optitons,
+        };
     }
-    return await next();
-}
 
-export const responsePing = (): MiddlewareHandler => async (c, next) => {
-    const interaction = await c.req.json<Interaction>();
-    if (interaction.type === 1) {
-        return c.json({ "type": 1 });
+    public setTitle(title: string) {
+        this.embed.title = title;
+        return this;
     }
-    return await next();
-}
 
-export const logger = (): MiddlewareHandler => async (c, next) => {
-    console.log(c.req.url);
-    return await next();
+    public setDescription(description: string) {
+        this.embed.description = description;
+        return this;
+    }
+
+    public setUrl(url: string) {
+        this.embed.url = url;
+        return this;
+    }
+
+    public setTimestamp(timestamp: string) {
+        this.embed.timestamp = timestamp;
+        return this;
+    }
+
+    public setColor(color: number) {
+        this.embed.color = color;
+        return this;
+    }
+
+    public setFooter(footer: APIEmbedFooter) {
+        this.embed.footer = footer;
+        return this;
+    }
+
+    public setImage(image: APIEmbedImage) {
+        this.embed.image = image;
+        return this;
+    }
+
+    public setThumbnail(thumbnail: APIEmbedThumbnail) {
+        this.embed.thumbnail = thumbnail;
+        return this;
+    }
+
+    public setAuthor(author: APIEmbedAuthor) {
+        this.embed.author = author;
+        return this;
+    }
+
+    public addField(field: APIEmbedField) {
+        if (this.embed.fields === undefined) {
+            this.embed.fields = [field];
+        }
+        this.embed.fields.push(field);
+        return this;
+    }
+
+    public removeField(index: number) {
+        if (this.embed.fields === undefined) {
+            return this;
+        }
+        this.embed.fields.splice(index, 1);
+        return this;
+    }
+
+    public insertField(index: number, field: APIEmbedField) {
+        if (this.embed.fields === undefined) {
+            this.embed.fields = [field];
+        }
+        this.embed.fields.splice(index, 0, field);
+        return this;
+    }
+
+    public setVideo(video: APIEmbedVideo) {
+        this.embed.video = video;
+        return this;
+    }
+
+    public build() {
+        return this.embed;
+    }
 }

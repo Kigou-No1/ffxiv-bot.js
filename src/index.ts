fc282b0cc5f6af8004e27593f07b29970fdd022e
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-04 10:31:26 +0000

    コマンドハンドラを実装

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import { Hono, MiddlewareHandler } from 'hono'
-import { InteractionType, verifyKey } from 'discord-interactions';
-import { Interaction } from './types/interaction';
-import { verifyRequest, responsePing, logger } from './utils';
+import { Hono, MiddlewareHandler } from "hono";
+import { InteractionType, verifyKey } from "discord-interactions";
+import { Interaction } from "./types/interaction";
+import { verifyRequest, responsePing, logger } from "./middleware";
+import { CommandManager } from "./types/command";
+import { commandNotFoundResponse } from "./utils";
+import { commandManager } from "./commands";
 
-const app = new Hono()
+const app = new Hono();
+const commands = commandManager;
+app.use(logger(), verifyRequest(), responsePing());
 
-app.use(logger(), verifyRequest(), responsePing())
+app.post("/", async c => {
+    const interaction = await c.req.json<Interaction>();
+    if (!interaction.data) return;
 
-app.post("/", async (c) => {
-  const interaction = await c.req.json<Interaction>();
-  if (!interaction.data) return
-
-  /* if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    switch (interaction.data.name) {
-      case "ping":
-
+    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+        const command = commands.getCommand(interaction.data.name.toLowerCase());
+        if (!command) {
+            return c.json(commandNotFoundResponse());
+        } else {
+            const response = await command.handler(c, interaction);
+            return c.json(response);
+        }
     }
-  } */
-})
+});
 
-
-
-export default app
+export default app;

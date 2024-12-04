ce2a0c2780ae993e3b8bc2a465f5ad2e1f6aa82a
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-04 11:43:49 +0000

    deferできるようにした

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { Hono, MiddlewareHandler } from "hono";
-import { InteractionType, verifyKey } from "discord-interactions";
+import { Hono } from "hono";
+import { InteractionResponseType, InteractionType } from "discord-interactions";
 import { Interaction } from "./types/interaction";
 import { verifyRequest, responsePing, logger } from "./middleware";
-import { CommandManager } from "./types/command";
 import { commandNotFoundResponse } from "./utils";
 import { commandManager } from "./commands";
 
@@ -14,14 +13,36 @@ app.post("/", async c => {
     const interaction = await c.req.json<Interaction>();
     if (!interaction.data) return;
 
-    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-        const command = commands.getCommand(interaction.data.name.toLowerCase());
-        if (!command) {
-            return c.json(commandNotFoundResponse());
-        } else {
-            const response = await command.handler(c, interaction);
-            return c.json(response);
-        }
+    const command = commands.getCommand(interaction.data.name.toLowerCase());
+    if (!command) {
+        console.log("Command not found: ", interaction.data.name);
+        console.log("Called type: ", interaction.type);
+        return c.json(commandNotFoundResponse());
+    }
+
+    switch (interaction.type) {
+        case InteractionType.APPLICATION_COMMAND:
+            const result = await command.handler(c, interaction);
+            const response = c.json(result.response);
+            if (result.deffered && command.deferHandler) {
+                await command.deferHandler(c, interaction);
+                return response;
+            }
+            break;
+        case InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE:
+            if (command.autocomplete_handler) {
+                const autocomplete = await command.autocomplete_handler(c, interaction);
+                return c.json(autocomplete);
+            } else {
+                console.log(
+                    "Command does not have an autocomplete handler: ",
+                    interaction.data.name,
+                );
+                return c.text("The command doesn't have an autocomplete handler", 501);
+            }
+        default:
+            console.log("Unhandled interaction type: ", interaction.type);
+            return c.text("Unhandled interaction type", 501);
     }
 });
 

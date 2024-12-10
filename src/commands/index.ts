6f1056b7372d40e8c1dc369a83c4c7542d2f307b
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-12-10 14:13:57 +0900

    unusedなインポートを削除

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,4 @@
-import { ApplicationCommandType } from "discord-api-types/v10";
-import { Command, CommandManager } from "../types/command";
+import { CommandManager } from "../types/command";
 import { ping } from "./ping";
 
 export const commandManager = new CommandManager([
